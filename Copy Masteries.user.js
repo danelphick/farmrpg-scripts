ee760d24a6995382fa242ffbe1efce4b8faf6b69
Dan Elphick <dabuek@gmail.com>
2026-07-26 14:33:10 +0100

    Add copy button to the inventory

--- Copy Masteries.user.js
+++ Copy Masteries.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copy Masteries
-// @version      1.0.1
-// @description  Creates a button top copy masteries from the FarmRPG Mastery Progress Page into clipboard
+// @version      1.1
+// @description  Creates buttons to copy masteries/inventory from the FarmRPG Mastery/Inventory pages to the clipboard.
 // @author       danelphick@
 // @match        https://farmrpg.com/index.php
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=farmrpg.com
@@ -9,53 +9,92 @@
 // @require      http://code.jquery.com/jquery-3.6.0.min.js
 // ==/UserScript==
 
-(function() {
-    /* globals jQuery, $, waitForKeyElements, navigation */
-    'use strict';
+(function () {
+  /* globals jQuery, $, waitForKeyElements, navigation */
+  "use strict";
 
-    $(document).ready(
-        () => {
-            function addButton(text, onclick, cssObj) {
-                // Check if button already exists and abort.
-                if ($("#copyMasteryButton").length > 0) return;
+  $(document).ready(() => {
+    function addButton(appendDiv, id, text, onclick, cssObj) {
+      // Check if button already exists and abort.
+      if ($("#" + id).length > 0 || !appendDiv) return;
 
-                let masteryTitle = null;
-                for (let x of $(".content-block-title")) { if (x.textContent== "Mastery In-Progress") { masteryTitle = x; break; } }
-                if (masteryTitle) {
-                    cssObj = cssObj || {position: 'relative', left:'4%', 'z-index': 3};
-                    let button = document.createElement('button');
-                    let btnStyle = button.style;
-                    // Add button so it appears to the right of "Mastery In-Progress"
-                    masteryTitle.appendChild(button);
-                    button.id = 'copyMasteryButton';
-                    button.innerHTML = text;
-                    button.onclick = onclick;
-                    Object.keys(cssObj).forEach(function (key) { btnStyle[key] = cssObj[key]; });
-                    return button;
-                }
-            }
+      let button = document.createElement("button");
+      let btnStyle = button.style;
+      // Add button so it appears to the right of "Mastery In-Progress"
+      appendDiv.appendChild(button);
+      button.id = id;
+      button.innerHTML = text;
+      button.onclick = onclick;
+      button.style = cssObj;
+      return button;
+    }
 
-            function click() {
-                let masteryTitle = null;
-                for (let x of $(".content-block-title")) { if (x.textContent.startsWith("Mastery In-Progress")) { masteryTitle = x; break; } }
+    function copyMasteryClick() {
+      let masteryTitle = findMasteryTitle();
 
-                if (masteryTitle) {
-                    // let nodes = $(".searchbar-found ul").splice(1, 2); nodes[1] = nodes[1].nextSibling;
-                    // document.getSelection().setBaseAndExtent(nodes[0], 0, nodes[1], 0)
-                    let nodes = $(".searchbar-found ul").splice(1, 2); nodes[1] = nodes[1].nextSibling;
-                    document.getSelection().setBaseAndExtent(masteryTitle, 0, masteryTitle.parentElement.lastElementChild, 0);
-                    GM_setClipboard(document.getSelection().toString(), "text", () => console.log("Clipboard set!"));
-                }
-            }
-            navigation.addEventListener("currententrychange", () => {
-                addGetSelectionButton();
-            });
+      if (masteryTitle) {
+        let nodes = $(".searchbar-found ul").splice(1, 2);
+        nodes[1] = nodes[1].nextSibling;
+        document
+          .getSelection()
+          .setBaseAndExtent(masteryTitle, 0, masteryTitle.parentElement.lastElementChild, 0);
+        GM_setClipboard(document.getSelection().toString(), "text", () =>
+          console.log("Mastery copied."),
+        );
+      }
+    }
 
-            function addGetSelectionButton() {
-                setTimeout(() => { addButton('Copy Mastery Data', click); }, 500);
-            }
+    function copyInventoryClick() {
+      let selectionDiv = $(".list-block-search.contacts-block")[0];
+      if (selectionDiv) {
+        let inventoryStats = $(selectionDiv).find(".content-block-title")[0]
+        document.getSelection().setBaseAndExtent(selectionDiv, 0, inventoryStats, 0);
+        GM_setClipboard(document.getSelection().toString(), "text", () =>
+          console.log("Inventory copied."),
+        );
+      }
+    }
 
-            addGetSelectionButton();
-        });
+    function findMasteryTitle() {
+      let masteryTitle = null;
+      for (let x of $(".content-block-title")) {
+        if (x.textContent.startsWith("Mastery In-Progress")) {
+          masteryTitle = x;
+          console.log("Found mastery title");
+          break;
+        }
+      }
+      return masteryTitle;
+    }
 
+    function findBlockAboveInventory() {
+      let inventoryList = $("[data-page=inventory].page .page-content .list-block")[0];
+      return inventoryList?.previousElementSibling.children[0];
+    }
+
+    function addCopyButtons() {
+      setTimeout(() => {
+        addButton(
+          findMasteryTitle(),
+          "copyMasteryButton",
+          "Copy Mastery Data",
+          copyMasteryClick,
+          "position: relative, left: 4%, z-index: 3",
+        );
+        addButton(
+          findBlockAboveInventory(),
+          "copyInventoryButton",
+          "Copy Inventory Data",
+          copyInventoryClick,
+          "position: absolute; top: 70%; right: 10px; z-index: 3;",
+        );
+      }, 500);
+    }
+
+    navigation.addEventListener("currententrychange", () => {
+      addCopyButtons();
+    });
+
+    addCopyButtons();
+  });
 })();
